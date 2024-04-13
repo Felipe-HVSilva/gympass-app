b5b8a524f40cea53749c6168039e53c39b5b734b
Felipe-HVSilva <felype.hen@hotmail.com>
2024-04-13 19:32:15 -0300

    feat: add controller check-ins

--- src/use-cases/factories/make-validate-check-in-use-case.ts
+++ src/use-cases/factories/make-validate-check-in-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 import { ValidateCheckInUseCase } from '../validate-check-in.use-case'
 
-export function makeValidateCheckInHistoryUseCase() {
+export function makeValidateCheckInUseCase() {
   const checkInsRepository = new PrismaCheckInsRepository()
   const useCase = new ValidateCheckInUseCase(checkInsRepository)
 
